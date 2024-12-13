34bbcbfae7dffb9c17b9c64633fa995c67f1eaab
Shire <im@ixu.me>
2024-12-13 15:31:02 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -40,7 +40,7 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main" "pkg"
 #UPDATE_PACKAGE "adguardhome" "sirpdboy/sirpdboy-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-adguardhome" "sirpdboy/sirpdboy-package" "main" "pkg"
+UPDATE_PACKAGE "luci-app-adguardhome" "imShire/openwrt-packages" "main" "pkg"
 
 if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
